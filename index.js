03a326a3bb0308ca8b38f115fd0a90b8772b4c7a
lucemery <lucemery316@gmail.com>
2024-05-03 17:44:53 -0500

    Update

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 function capitalize(someWord) {
     // your code here
+
+    // * initialize firstLetter variable
+    // * assign it the value of first index of someWord
+    
+        // * initialize restOfWord variable
+        // * assign it the value of the rest of the string (someWord)
+        // * initialize fullWord variable
+        // * concatenate firstLetter capitalized with restOfWord lowercase
+        // * return fullWord
+let firstLetter = someWord[0].toUpperCase();
+
+let restOfWord = someWord.slice(1).toLowerCase();
+
+let fullWord = firstLetter + restOfWord;
+
+    return fullWord;
 }
 
 console.log(capitalize("tomato")); // should print 'Tomato'
